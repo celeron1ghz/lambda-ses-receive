92d7ca36d73985907485f144ab87113485e2ac5f
celeron1ghz <celeron1ghz@gmail.com>
2018-01-22 23:29:25 +0900

    using terraform s3 backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,15 @@
 provider "aws"    { region = "us-east-1" }
 
+terraform {
+  required_version = ">= 0.9.0"
+
+  backend "s3" {
+    bucket  = "acceptessa-tfstate"
+    key     = "lambda-ses-receive.tfstate"
+    region  = "ap-northeast-1"
+  }
+}
+
 resource "aws_ses_receipt_rule_set" "ruleset" {
 	rule_set_name = "default-rule-set"
 }
